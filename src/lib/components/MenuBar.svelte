851a1f5d0e85e93f98e43aa21ab8bf69e9ec262b
Matthew Tran <matran@bushelpowered.com>
2024-03-10 22:45:51 -0500

    Add icons.

--- src/lib/components/MenuBar.svelte
+++ src/lib/components/MenuBar.svelte
@@ -3,11 +3,13 @@
 </script>
 
 <div class="grid grid-cols-3 gap-4">
-	<Button.Root class="inline-flex h-12 items-center justify-center bg-blue hover:bg-blue/80">
+	<Button.Root class="inline-flex h-12 items-center justify-center bg-blue-300 hover:bg-blue/80">
+		<div class="i-tabler-arrow-left" />
 		Back
 	</Button.Root>
 	<input type="text" placeholder="Type a name of a pokemon to search..." />
-	<Button.Root class="inline-flex h-12 items-center justify-center bg-blue hover:bg-blue/80">
+	<Button.Root class="inline-flex h-12 items-center justify-center bg-blue-300 hover:bg-blue/80">
 		Next
+		<div class="i-tabler-arrow-right" />
 	</Button.Root>
 </div>
