07449a5d4686088a3d83c07391b7114d578f6322
Matthew Tran <matran@bushelpowered.com>
2024-03-11 12:26:28 -0500

    Add comments.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import type { PageData } from './$types';
 	import { createQuery } from '@tanstack/svelte-query';
+	import type { PageData } from './$types';
 	import PokemonGrid from '$lib/components/PokemonGrid.svelte';
 	import { getPokemon } from '$lib/api/pokemon';
 
+	// Get page props from SSR
 	export let data: PageData;
 	const pokemon = data?.pokemon?.results;
 	const offset = data?.offset ?? '0';
