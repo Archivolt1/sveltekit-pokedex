03fba7a6462a4c1ffea98d6ec5ff97bd61016e5b
Matthew Tran <matran@bushelpowered.com>
2024-03-12 00:30:50 -0500

    Reduce FOUC.

--- src/lib/components/PokemonCardPlaceholder.svelte
+++ src/lib/components/PokemonCardPlaceholder.svelte
@@ -4,7 +4,7 @@
 
 <div class="bg-gray-100 rounded overflow-hidden">
 	<div class="px-6 py-4">
-		<div class="font-bold text-sm md:text-lg mb-2">Loading...</div>
+		<div class="font-bold text-sm md:text-lg mb-2" />
 		<div class="w-full h-96px md:h-128px" />
 		<PokemonTypeBadge type="" />
 	</div>
