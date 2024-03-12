50058081a7b60dc865820753c0faaa4d1f743f50
Matthew Tran <matran@bushelpowered.com>
2024-03-12 00:18:17 -0500

    Alter text color.

--- src/lib/components/PokemonTypeBadge.svelte
+++ src/lib/components/PokemonTypeBadge.svelte
@@ -3,5 +3,7 @@
 </script>
 
 <div class="overflow-hidden rounded-full text-center my-1">
-	<div class="font-bold text-xs py-1">{type.charAt(0).toLocaleUpperCase() + type.slice(1)}</div>
+	<div class="font-bold text-xs py-1 text-neutral">
+		{type.charAt(0).toLocaleUpperCase() + type.slice(1)}
+	</div>
 </div>
